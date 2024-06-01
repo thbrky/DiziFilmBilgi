65be776d786277a5377d35b95644af3f3e27259a
Taha Berkay Redifoğlu <86873168+thbrky@users.noreply.github.com>
2024-06-01 16:21:07 +0300

    Add Files via upload

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authControllers from "../controllers/authControllers.js";
-
-const router = express.Router();
-
-router.post("/register", authControllers.register);
-router.post("/login", authControllers.login);
-
-export default router;
